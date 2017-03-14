ff0e2ac2b6c230fe8387585d1f94613b8256fa03
Vandeth Nop <vnop14@gmail.com>
2017-03-14 10:22:30 -0700

    testing something

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 var app = require('./server');
-var port = process.env.PORT || 3000;
+// var port = process.env.PORT || 3000;
+var port = 3000;
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
