3cca2d492b54f7f650ec865bdf5d25dbee96275c
Vandeth Nop <vnop14@gmail.com>
2017-03-14 13:30:58 -0700

    added dummy data to client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import { Router, Route, Redirect } from 'react-router';
 
+var dummyData = [
+  {
+    task: 'work on client side',
+    details: 'have client add a task',
+    id: 1
+  },
+  {
+    task: 'work on server side',
+    details: 'have server send data to client',
+    id: 2
+  },
+  {
+    task: 'work on database',
+    details: 'create schema, model, and initiate it',
+    id: 3
+  }
+];
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -10,7 +28,7 @@ class App extends React.Component {
 
   render() {
     return (<div>
-      <h1>What up</h1>
+      <h1>Do List</h1>
       </div>)
   }
 }
