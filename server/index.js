2c553e133498def7c2334128ff8ba053d001e332
Vandeth Nop <vnop14@gmail.com>
2017-03-14 10:30:01 -0700

    reverting, test confirmed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 var app = require('./server');
-// var port = process.env.PORT || 3000;
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
