122d54b0d2c762dad935ea45afe18ea22b9c6c32
Vandeth Nop <vnop14@gmail.com>
2017-03-13 21:03:17 -0700

    added process env port for server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var app = require('./server');
 // add process env port later
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
